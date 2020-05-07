1b44f2abf98ec66b0c9987344d6223dca91708f3
krishn3012 <krishngupta3012@gmail.com>
2020-05-07 20:29:06 +0530

    car racing game

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,32 @@ class Game {
       form.display();
     }
   }
+
+  play(){
+    form.hide();
+    text("GAME START",120,100);
+    Player.getPlayerInfo();
+    if (allPlayers!==undefined){
+      var displayPos = 130
+      for (var plr in allPlayers){
+        if (plr==="player"+player.index){
+          fill("red");
+        }
+        else{
+          fill("black");
+        }
+        text(allPlayers[plr].name + " : "+ allPlayers[plr].distance, 120,displayPos);
+        displayPos = displayPos + 20;
+        
+      }
+    }
+
+    if ((keyIsDown(UP_ARROW)) && (player.index!==null)){
+      player.distance+=10;
+      player.update();
+    }
+
+  }
+
+
 }
--- js/Player.js
+++ js/Player.js
@@ -1,5 +1,10 @@
 class Player {
-  constructor(){}
+  constructor(){
+    this.name=null;
+    this.distance= 0;
+    this.index = null;
+
+  }
 
   getCount(){
     var playerCountRef = database.ref('playerCount');
@@ -14,10 +19,19 @@ class Player {
     });
   }
 
-  update(name){
-    var playerIndex = "player" + playerCount;
+  update(){
+    var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).set({
-      name:name
+      name:this.name,
+      distance:this.distance
     });
   }
+
+  static getPlayerInfo(){
+    var playerInfoRef=database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers=data.val()
+    });
+  }
+
 }
